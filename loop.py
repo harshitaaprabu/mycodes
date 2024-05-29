2ec3a336bf782dda0063ac0b34f1d2b34bf9ee11
Harshitaa Prabu <142102708+harshitaaprabu@users.noreply.github.com>
2024-05-29 19:28:18 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,38 @@
+# name="Harshitaa"
+# if name:
+#     print(name)
+# else:
+#     print("Not found")
+
+
+# if else loop
+# num1=int(input("Enter num1 :"))
+# num2=int(input("Enter num2 : "))
+# if num1>num2:
+#     print(num1,"is greater than",num2)
+# elif num1<num2:
+#     print(num1,"is less than",num2)
+# elif num1==num2:
+#     print(num1,"is equal to",num2)
+# else:
+#     print("Invalid data type")/
+
+
+# for loop
+num_list=[1,2,3,4,"Name",6,7,8,9,10]
+# for i in num_list:
+#     print(i)
+    # print(i,end="")
+    # if i=="Name":
+    #     print(i)
+
+
+# for idx in range(len(num_list)):
+#     print(idx)
+#     if idx==4:
+#         print(num_list[4])    
+
+
+
+# for idx,value in enumerate(num_list):
+#     print(idx,value)
